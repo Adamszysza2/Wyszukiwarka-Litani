5169037638dcbca144201e26ce40d4a1b18656f8
Adamszysza2 <adamszyszka2@gmail.com>
2026-04-11 13:45:31 +0200

    wersja 2.1

--- generation.py
+++ generation.py
@@ -1,16 +1,1 @@
-import json
-with open(r"C:/Users/adams/OneDrive/Programowane/javaskirpt/html/Strona z Litaniami/tresc_litani.txt", "r", encoding="utf-8") as f:
-    surowe_linie = f.readlines()
-
-przetworzone_linie = []
-for linia in surowe_linie:
-    # Usuwamy niechciane znaki: [, ], ' oraz zbędne białe znaki
-    oczyszczona = linia.replace("[", "").replace("]", "").replace("'", "").strip()
-    
-    if oczyszczona: # Dodajemy do wyniku tylko niepuste linie
-        if "Boskie Dzieciątko" in oczyszczona and "zmiłuj się nad nami" not in oczyszczona:
-            oczyszczona += " - zmiłuj się nad nami"
-        przetworzone_linie.append(oczyszczona)
-
-for wynik in przetworzone_linie:
-    print(wynik)
+text = input("Poadaj tresc litani")
